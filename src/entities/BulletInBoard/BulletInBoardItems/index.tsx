781a935362826915ca528ed59703ba1ac6382151
anikulshin310 <anikulshin310@gmail.com>
2022-06-11 13:03:47 +0000

    use pagination testing

--- src/entities/BulletInBoard/BulletInBoardItems/index.tsx
+++ src/entities/BulletInBoard/BulletInBoardItems/index.tsx
@@ -2,24 +2,42 @@ import React, { FC, useEffect } from "react";
 import style from "./BulletInBoardItems.module.scss";
 import { IData } from "../../../mocks/data";
 import SearchInput from "../../../components/SearchInput";
-import Pagination from "../../../components/Pagination";
+import usePagination from "../../../hooks/usePagination";
 
 interface IBulletInBoardItems {
   items: IData[];
 }
 
 const BulletInBoardItems: FC<IBulletInBoardItems> = ({ items }) => {
-  useEffect(() => console.log(items));
+
+  const {
+    totalPages,
+    nextPage,
+    prevPage,
+    setPage,
+    firstContentIndex,
+    lastContentIndex,
+    page,
+  } = usePagination({ contentPerPage: 6, count: items.length });
+  
   return (
     <>
-      {" "}
+      
       <div className={style.items_wrapper}>
         <SearchInput
           onChange={(e) => console.log(e.target.value)}
           placeHolder={"Найти объявление"}
         />
       </div>
-      <Pagination contentPerPage={8} count={items.length}></Pagination>
+      <div onClick={prevPage}>prev</div>
+      <div onClick={nextPage}>next</div>
+      
+      {items
+    .slice(firstContentIndex, lastContentIndex)
+    .map((el: any) => (
+      <div className="item">{el.name}</div>
+   ))}
+      
     </>
   );
 };
