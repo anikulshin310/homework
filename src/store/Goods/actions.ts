882518ecd8e7ad456e4577a0736b7faa8a5b767b
anikulshin310 <anikulshin310@gmail.com>
2022-06-29 12:50:48 +0000

    addItem

--- src/store/Goods/actions.ts
+++ src/store/Goods/actions.ts
@@ -5,3 +5,8 @@ export const DELETE_ITEM = 'DELETE_ITEM';
 export const deleteItem = (payload: IData) => {
   return { type: DELETE_ITEM, payload };
 };
+
+export const ADD_ITEM ='ADD_ITEM'
+export const addItem =(payload:IData)=>{
+  return {type:ADD_ITEM, payload}
+}
