066a73268d9a879727113c6a04a34b11d6fcc13b
anikulshin310 <anikulshin310@gmail.com>
2022-06-29 12:50:58 +0000

    addItem

--- src/store/Goods/reducer.ts
+++ src/store/Goods/reducer.ts
@@ -1,5 +1,5 @@
 import { dataItems } from '../../mocks/data';
-import { DELETE_ITEM } from './actions';
+import { DELETE_ITEM, ADD_ITEM } from './actions';
 
 const initialState = dataItems;
 
@@ -7,6 +7,8 @@ export const GoodsReducer = (state = initialState, action: any) => {
   switch (action.type) {
     case DELETE_ITEM:
       return [...state.filter((item) => item !== action.payload)];
+    case ADD_ITEM:
+      return [action.payload, ...state];
 
     default:
       return state;
