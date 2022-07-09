28839dc5aee26394fa946dfe440b19c1555227ae
Anikulshin310 <anikulshin310@gmail.com>
2022-07-09 12:30:40 +0300

    fix

--- src/store/Goods/actions.ts
+++ src/store/Goods/actions.ts
@@ -14,23 +14,3 @@ export const EDIT_ITEM = 'EDIT_ITEM';
 export const editItem = (payload: IData) => {
   return { type: EDIT_ITEM, payload };
 };
-
-export const SET_CURRENT = 'SET_CURRENT';
-export const setCurrentItem = (payload: any) => {
-  return { type: SET_CURRENT, payload };
-};
-
-export const CLEAR_CURRENT = 'CLEAR_CURRENT';
-export const clearCurrent = () => {
-  return { type: CLEAR_CURRENT };
-};
-
-export const HANDLE_INPUT_TEXT = 'HANDLE_INPUT_TEXT';
-export const handleInputText = (payload: any, field: string) => {
-  return { type: HANDLE_INPUT_TEXT, payload, field };
-};
-
-export const HANDLE_UUID = 'HANDLE_UUID';
-export const handleUuid = (payload: any, field: string) => {
-  return { type: HANDLE_UUID, payload, field };
-};
